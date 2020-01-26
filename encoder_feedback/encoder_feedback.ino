dffa00964af1db50c3636ad2cad6952acaeeefd2
Michael Milic <mikemilic98@gmail.com>
2020-01-25 19:36:38 -0800

    encoder and pulsing from scratch no stepper library

--- encoder_feedback/encoder_feedback.ino
+++ encoder_feedback/encoder_feedback.ino
@@ -2,13 +2,13 @@
 #define CW 0
 #define CCW 1
 
-#include <Stepper.h>
-#include<Wire.h>
+#define A        8                     // the pin connected to the wire A of the coil A (or to the H-bridge pin controlling the same wire) 
+#define A_bar    9                     // the pin connected to the wire A- of the coil A (or to the H-bridge pin controlling the same wire)
+#define B        10                    // the pin connected to the wire B of the coil A (or to the H-bridge pin controlling the same wire)
+#define B_bar    11                     // the pin connected to the wire B- of the coil A (or to the H-bridge pin controlling the same wire)
+#define x        5000                  // smaller values may make the motor produce more speed and less torque
+#define stepsPerRevolution 200         // you can the number of steps required to make a complete revolution in the data sheet of your motor
 
-// Define number of steps per revolution:
-const int stepsPerRevolution = 200;
-// Initialize the stepper library on pins 8 through 11:
-Stepper myStepper = Stepper(stepsPerRevolution, 8, 9, 10, 11);
 
 const int a = 2;
 const int b = 3;
@@ -21,34 +21,24 @@ volatile int prev_pos = 0;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  //sensor
   pinMode(a, INPUT);
   pinMode(b, INPUT); 
   //detect_a = digitalRead(a);
   //detect_b = digitalRead(b);
-  attachInterrupt(digitalPinToInterrupt(a), ISR_A, RISING);
-  //attachInterrupt(digitalPinToInterrupt(b), ISR_B, RISING);  
-
-// Set the motor speed (RPMs):
-  myStepper.setSpeed(100);
+  attachInterrupt(digitalPinToInterrupt(a), ISR_A, RISING); 
+  //motor
+  pinMode(A, OUTPUT);
+  pinMode(A_bar, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(B_bar, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(cur_pos != prev_pos)
-  {
-    Serial.print(cur_pos);
-    Serial.print("\t");
-    Serial.print(dir == CW ? "clockwise" : "counter-clockwise");
-    Serial.print("\t");
-    Serial.println(angle);
-    prev_pos = cur_pos;
-  }
-  myStepper.step(200);
-  delay(500);
 
- if(cur_pos != prev_pos)
-  {
+  if (cur_pos != prev_pos){
     Serial.print(cur_pos);
     Serial.print("\t");
     Serial.print(dir == CW ? "clockwise" : "counter-clockwise");
@@ -56,9 +46,30 @@ void loop() {
     Serial.println(angle);
     prev_pos = cur_pos;
   }
-  //Step on revolution in the other direction:
-  myStepper.step(-200);
-  delay(500);
+
+  digitalWrite(A, HIGH);
+  digitalWrite(A_bar, LOW);
+  digitalWrite(B, LOW); 
+  digitalWrite(B_bar, HIGH);
+  delayMicroseconds (x);
+
+  digitalWrite(A, LOW);
+  digitalWrite(A_bar, HIGH);
+  digitalWrite(B, LOW);
+  digitalWrite(B_bar, HIGH);
+  delayMicroseconds (x);
+
+  digitalWrite(A, LOW);
+  digitalWrite(A_bar, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(B_bar, LOW);
+  delayMicroseconds (x);
+
+  digitalWrite(A, HIGH);
+  digitalWrite(A_bar, LOW);
+  digitalWrite(B, HIGH);
+  digitalWrite(B_bar, LOW);
+  delayMicroseconds (x);
 
 }
 
